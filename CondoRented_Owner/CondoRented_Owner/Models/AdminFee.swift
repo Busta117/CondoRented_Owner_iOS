f4720fc180444a4f777ce4a1e1aa9b41edf46526
Santiago Bustamante <santiagobustamante@Capitoles-MacBook-Pro.local>
2024-10-25 08:51:33 +0200

    listingid in admin fee

--- CondoRented_Owner/CondoRented_Owner/Models/AdminFee.swift
+++ CondoRented_Owner/CondoRented_Owner/Models/AdminFee.swift
@@ -19,7 +19,11 @@ class AdminFee: CodableAndIdentifiable {
     var percent: Double
     var admin: Admin?
     
-    init(id: String = UUID().uuidString, 
+    var listingId: String?
+    var adminId: String?
+    
+    @available(*, deprecated, renamed: "init", message: "use init with ids")
+    init(id: String = UUID().uuidString,
          listing: Listing?,
          dateStart: Date,
          dateFinish: Date? = nil,
@@ -34,34 +38,48 @@ class AdminFee: CodableAndIdentifiable {
         self.admin = admin
     }
     
+    init(id: String = UUID().uuidString,
+         listingId: String?,
+         dateStart: Date,
+         dateFinish: Date? = nil,
+         percent: Double,
+         adminId: String? = nil) {
+        
+        self.id = id
+        self.listingId = listingId
+        self.dateStart = dateStart
+        self.dateFinish = dateFinish
+        self.percent = percent
+        self.adminId = adminId
+    }
+    
     enum CodingKeys: CodingKey {
         case id
-//        case listing
         case dateStart
         case dateFinish
         case percent
-        case admin
+        case listingId
+        case adminId
     }
     
     required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         id = try container.decode(String.self, forKey: .id)
-//        listing = try container.decodeIfPresent(Listing.self, forKey: .listing)
-//        listing = try container.decodeIfPresent(Listing.self, forKey: .listing, configuration: .decodingConfiguration)
         dateStart = try container.decode(Date.self, forKey: .dateStart)
         dateFinish = try container.decodeIfPresent(Date.self, forKey: .dateFinish)
         percent = try container.decode(Double.self, forKey: .percent)
-        admin = try container.decodeIfPresent(Admin.self, forKey: .admin)
+        adminId = try container.decodeIfPresent(String.self, forKey: .adminId)
+        listingId = try container.decodeIfPresent(String.self, forKey: .listingId)
     }
     
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(id, forKey: .id)
-//        try container.encodeIfPresent(listing, forKey: .listing, configuration: .encodingConfiguration)
         try container.encode(dateStart, forKey: .dateStart)
         try container.encodeIfPresent(dateFinish, forKey: .dateFinish)
         try container.encode(percent, forKey: .percent)
-        try container.encodeIfPresent(admin, forKey: .admin)
+        try container.encodeIfPresent(adminId, forKey: .adminId)
+        try container.encodeIfPresent(listingId, forKey: .listingId)
     }
     
     static func fetchAll(modelContext: ModelContext) -> [AdminFee] {
